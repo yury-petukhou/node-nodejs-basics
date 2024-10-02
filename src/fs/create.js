f979b2ab350b026524daea9f3a911a4803343540
yury-petukhou <petukhov.y1991@gmail.com>
2024-10-02 16:14:33 +0200

    developed create func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,31 @@
-const create = async () => {
-    // Write your code here 
-};
+import { promises as fs } from 'fs';
+import path from 'path';
 
-await create();
+const create = async () => {
+    const directoryPath = 'files';
+    const fileName = 'fresh.txt';
+    const fullPath = path.join(directoryPath, fileName);
+
+    try {
+        await fs.access(fullPath);
+        throw new Error('FS operation failed');
+    } catch (error) {
+       
+        if(error.code === 'ENOENT') {
+            try {
+                await fs.writeFile(fullPath, 'I am fresh and young');
+                console.log('File created successfully');
+            } catch (writeErr) {
+                console.error('Failed to create file:', writeErr);
+            }
+        
+        }  else {
+            throw new Error('FS operation failed');
+        }
+
+  };
+
+}
+
+
+await create()
