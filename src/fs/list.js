21e85503ec2ef2ae128d5e78616261f75dcd4a2b
yury-petukhou <petukhov.y1991@gmail.com>
2024-10-03 15:22:42 +0200

    Developed List

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
-const list = async () => {
-    // Write your code here 
-};
+import path from 'path';
+import {promises as fs} from 'fs';
 
-await list();
+const list = async (pathToDir) => {
+    // Write your code here 
+try {
+    const files = await fs.readdir(pathToDir);
+    console.log(files);
+    return files;
+} catch (error) {
+
+    if (error.code === 'ENOENT') {
+        throw new Error('FS operation failed');
+    }
+
+    console.error(`list failed: ${error}`);
+    throw error;
+}
+};
+const pathToDir = path.join('fs/files');
+await list(pathToDir);
