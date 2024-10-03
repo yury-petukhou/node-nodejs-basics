2f3dbfbba9d661b8484296ec6eea85182c3467c9
yury-petukhou <petukhov.y1991@gmail.com>
2024-10-03 15:27:01 +0200

    Developed read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
-const read = async () => {
-    // Write your code here 
-};
+import {promises as fs} from 'fs';
+import path from 'path';
 
-await read();
+const read = async (file, format) => {
+    // Write your code here 
+    try {
+       const content = await fs.readFile(file,format);
+        console.log(content);
+        return content;
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+
+        console.error(`read failed: ${error}`);
+    }
+};
+const pathToFile = path.join('fs/files', 'fileToRead.txt');
+await read(pathToFile, 'utf8');
