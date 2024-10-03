6bfe13f51a709690e5e1f60050e017ba223ac892
yury-petukhou <petukhov.y1991@gmail.com>
2024-10-03 19:00:59 +0200

    Developed args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
     // Write your code here 
+   let args = ''
+   process.argv.slice(2).forEach(arg => {
+        if (arg.startsWith('--')) {
+            args += `${arg.slice(2)} is ${process.argv[process.argv.indexOf(arg) + 1]} `
+        }
+    });
+
+    console.log(args)
 };
 
 parseArgs();
