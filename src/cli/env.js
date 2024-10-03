370d2672280a555eee6754ddff24735cf160fa23
yury-petukhou <petukhov.y1991@gmail.com>
2024-10-03 18:50:56 +0200

    Developed env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
     // Write your code here 
+    const rssVariables = Object.entries(process.env)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join(' ');
+        console.log(rssVariables)
 };
+process.env.RSS_TEST1 = 'value1';
+process.env.RSS_TEST2 = 'value2';
+process.env.OTHER_VAR = 'not included';
 
 parseEnv();
