b80ab3de05da6b6a6d9cc06f49dafc07cd96ccd5
yury-petukhou <petukhov.y1991@gmail.com>
2024-10-03 15:17:48 +0200

    Developed delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
-const remove = async () => {
+import {promises as fs} from 'fs';
+import path from 'path';
+const remove = async (file) => {
     // Write your code here 
+
+    try {
+        await fs.access(file);
+        await fs.unlink(file);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            console.error(`delete failed: ${err}`);
+            throw err;
+        }
+    }
 };
 
-await remove();
+const pathToDelete = path.join('fs/files', 'fileToRemove.txt');
+await remove(pathToDelete);
