70ccf8ea65ed89e1f7e958ec2adf180da352e1cf
yury-petukhou <petukhov.y1991@gmail.com>
2024-10-03 15:04:55 +0200

    Developed rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,33 @@
-const rename = async () => {
-    // Write your code here 
-};
+import { promises as fs } from 'fs';
+import path from 'path';
 
-await rename();
+const rename = async (from, to) => {
+    // Write your code here 
+    const directory = path.dirname(from);
+    try {
+
+       try {
+        await fs.access(from);
+        console.log('File found', from);
+       } catch (error) {
+         throw new Error('FS operation failed');
+       } 
+
+       try {
+        await fs.access(to);
+        throw new Error('FS operation failed');
+       } catch (error) {
+            if(error.code === 'ENOENT') {
+                await fs.rename(from, to);
+                console.log('File renamed successfully',from, "-->", to);
+            } else {
+                throw new Error('FS operation failed');
+            }
+       } 
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
+};
+const oldPath =  'fs/files/wrongFilename.txt';
+const newPath = 'fs/files/properFilename.md';
+await rename(oldPath, newPath);
