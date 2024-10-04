5961bb35883f33fa953eff3da62ae723cc24cc95
yury-petukhou <petukhov.y1991@gmail.com>
2024-10-04 16:11:53 +0200

    Developed calculateHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
-const calculateHash = async () => {
+import { open } from 'fs/promises';
+import { pipeline } from 'stream/promises';
+import crypto from 'crypto';
+
+const calculateHash = async (filePath) => {
+    let fileHandle;
+    
     // Write your code here 
+    try {
+        fileHandle = await open(filePath, 'r');
+        const readStream = fileHandle.createReadStream();
+        const hash = crypto.createHash('sha256');
+        await pipeline(readStream, hash)
+        console.log(hash.digest('hex')) 
+    }  finally {
+        if (fileHandle) await fileHandle.close();
+    }
 };
 
-await calculateHash();
+await calculateHash('hash/files/fileToCalculateHashFor.txt');
