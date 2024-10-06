e2de791fb328fc8b887037a3c9ff29d140b5e193
yury-petukhou <petukhov.y1991@gmail.com>
2024-10-06 18:21:17 +0200

    Added transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,39 @@
-const transform = async () => {
-    // Write your code here 
+import fs from 'fs';
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
+const readFromStdin = () => {
+    console.log("Enter the text (Ctrl+D to finish):");
+
+    return process.stdin;
 };
 
-await transform();
+const transform = async  (modificator) => {
+    // Write your code here 
+    try {
+        await pipeline(
+            readFromStdin(),
+            new Transform({
+                transform(chunk, encoding, callback){
+                    const result =  modificator ? modificator(chunk.toString()) : chunk.toString()
+                    this.push(result);
+                    callback()
+                }
+            }),
+            new Transform({
+                transform(chunk, encoding, callback) {
+                    // Add the new line 
+                    this.push(chunk + '\n');
+                    callback();
+                }
+            }),
+            process.stdout
+        )
+    } catch (error) {
+        throw new Error(error)
+    } finally {
+        process.exit();
+    }
+};
+
+await transform((v) => v.split('').reverse().join(''));
