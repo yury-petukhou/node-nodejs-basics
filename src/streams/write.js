29c1f99a3ae8dcae1dd3a996684d03487e88140a
yury-petukhou <petukhov.y1991@gmail.com>
2024-10-06 18:20:55 +0200

    Add write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,30 @@
-const write = async () => {
-    // Write your code here 
-};
+import { createWriteStream } from 'fs';
 
-await write();
+const write = async (path) => {
+    // Write your code here 
+    const writeStream = createWriteStream(path);
+    let result = ''
+    console.log("Enter the text (Ctrl+D to finish):");
+
+    process.stdin.on('data', (chunk) => {
+        result += chunk.toString().trim()
+    });
+
+    process.stdin.on('end', () => {
+        console.log('--- Enter is  done ---');
+        writeStream.write(result)
+        writeStream.end();
+
+        writeStream.on('finish', () => {
+            console.log('Writting is  done.');
+        });
+    
+        writeStream.on('error', (error) => {
+            console.error('Writting error:', error);
+        });
+    });
+    
+};
+ 
+const pathToWriteFile = 'streams/files/fileToWrite.txt';
+await write(pathToWriteFile);
