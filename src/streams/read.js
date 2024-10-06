f8a18f511fe191c0bd1f24e4fd6db6c7417aa313
yury-petukhou <petukhov.y1991@gmail.com>
2024-10-06 18:20:44 +0200

    Added read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
-const read = async () => {
-    // Write your code here 
+import { createReadStream } from 'fs';
+const read = async (path) => {
+    // Write your code here
+    const readStream =  createReadStream(path);
+    let result = '';
+ 
+    readStream.on('data', (chunk) => {
+     result += chunk;
+    });
+    
+    readStream.on('end', () => {
+        console.log('--- got all data of file ---');
+        process.stdout.write(result)
+    });
+
+    readStream.on('error', (error) => {
+      console.log(error);
+    });
+
+  
 };
 
-await read();
+const pathToFile = 'streams/files/fileToRead.txt';
+await read(pathToFile);
